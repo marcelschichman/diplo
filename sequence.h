089343ab12e6dac6e6d6a24e5a2f7229c2ae3931
Marcel Schichman <marcel.schichman@gmail.com>
2016-11-14 20:50:46 +0100

    added sequence from bakalarka

---
+++ sequence.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <fstream>
+#include <algorithm>
+
+class Sequence {
+public:
+    Sequence();
+    Sequence(const std::string& _data, std::string _id = std::string());
+    Sequence(const Sequence& orig, bool remapReverse = false);
+    virtual ~Sequence();
+
+    char* ToDalignFromat();
+    const std::string &GetData() const {
+        return data;
+    }
+    const std::string &GetId() const {
+        return id;
+    }
+
+private:
+    char* dalignFromat;
+    std::string data;
+    std::string id;
+};
+
+class FASTA {
+public:
+    FASTA(const std::string& _filename);
+
+    FASTA& operator>>(Sequence& seq);
+private:
+    std::string filename;
+};
+
+class FASTQ {
+public:
+    FASTQ(const std::string& filename);
+
+    FASTQ& operator>>(Sequence& seq);
+
+    operator bool() const {
+        return isOk;
+    };
+private:
+    std::ifstream is;
+    bool isOk;
+};
+
+enum Direction {
+    EDIR_FORWARD,
+    EDIR_BACKWARD
+};
